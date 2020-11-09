4fc40d8b0feaeb0bc884fdd8a15bdc19a5535069
Ksenia Tsubulko <tsubulko.ksenia@gmail.com>
2020-11-09 20:06:28 +0300

    Added extension methods

--- TestableLibrary/ExtClass.cs
+++ TestableLibrary/ExtClass.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestableLibrary
+{
+    public static class ExtClass
+    {
+        public static int ExtMethod(this MyClass myClass, int param)
+        {
+            return param;
+        }
+    }
+}
