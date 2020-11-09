d54179d88ff40dd60341c98e50d24dc482e42a19
Ksenia Tsubulko <tsubulko.ksenia@gmail.com>
2020-11-09 20:23:12 +0300

    New test classes

---
+++ TestableLibrary/JustClass.cs
@@ -0,0 +1,22 @@
+﻿namespace TestableLibrary.JustNamspace
+{
+    public class JustClass
+    {
+        private int foo;
+        public string boo;
+        protected bool goo;
+
+        public JustClass(int foo, string boo, bool goo)
+        {
+            this.foo = foo;
+            this.boo = boo;
+            this.goo = goo;
+        }
+
+        public int Foo
+        {
+            get => foo;
+            set => foo = value;
+        }
+    }
+}
