23cf0408ab2c897f16ad840e9a7438a008330cde
Ksenia Tsubulko <tsubulko.ksenia@gmail.com>
2020-11-10 09:10:30 +0300

    Update Unit tests

---
+++ AssemblyBrowserLib.Tests/AssemblyBrowser.Test.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Reflection;
+using NUnit.Framework;
+
+namespace AssemblyBrowserLib.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        private readonly IAssemblyBrowser _assemblyBrowser = new AssemblyBrowser();
+
+        private string _testLocation = (Assembly.Load("TestableLibrary")).Location;
+
+        [Test]
+        public void NamespaceTest()
+        {
+            var location = Assembly.GetExecutingAssembly().Location;
+            var namespaces = _assemblyBrowser.GetNamespaces(location);
+            var _namespace = namespaces[0];
+            var currentNamespace = Assembly.GetExecutingAssembly().GetTypes()[0].Namespace;
+            Assert.AreEqual(_namespace.DeclarationName, currentNamespace);
+        }
+
+        [Test]
+        public void TypesTest()
+        {
+            var location = Assembly.GetExecutingAssembly().Location;
+            var namespaces = _assemblyBrowser.GetNamespaces(location);
+            var _namespace = namespaces[0];
+            var types = _namespace.Members;
+            Assert.AreEqual(Assembly.GetExecutingAssembly().GetTypes().Length, types.Count);
+        }
+
+        [Test]
+        public void TypesNameTest()
+        {
+            var namespaces = _assemblyBrowser.GetNamespaces(_testLocation);
+            foreach (var _namespace in namespaces)
+            {
+                if (_namespace.DeclarationName != "TestableLibrary" &&
+                    _namespace.DeclarationName != "TestableLibrary.Another"&&
+                    _namespace.DeclarationName != "TestableLibrary.JustNamspace")
+                {
+                    Assert.Fail($"Error in namespace name {_namespace.DeclarationName}");
+                }
+            }
+        }
+
+        [Test]
+        public void ExtensionMethodName()
+        {
+            var namespaces = _assemblyBrowser.GetNamespaces(_testLocation);
+            var types = namespaces[0].Members;
+            foreach (var type in types)
+            {
+                if (type.Name == "MyClass")
+                {
+                    bool flag = false;
+                    ;
+                    foreach (var member in ((ContainerInfo) type).Members)
+                    {
+                        if (member.Name == "ExtMethod")
+                        {
+                            flag = true;
+                        }
+                    }
+
+                    Assert.IsTrue(flag);
+                }
+            }
+        }
+
+        [Test]
+        public void TypeNamesTest()
+        {
+            var namespaces = _assemblyBrowser.GetNamespaces(_testLocation);
+            var types = namespaces[0].Members;
+            foreach (var type in types)
+            {
+                if (type.Name != "MyClass" && type.Name != "MyClassAnother" && type.Name != "ExtClass")
+                {
+                    Assert.Fail($"Error in type name {type.Name}");
+                }
+
+            }
+        }
+    }
+}
---
+++ AssemblyBrowserLib.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AssemblyBrowserLib.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AssemblyBrowserLib.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2FFF4B81-CEEA-49D6-9B1F-E63FE4793F9A")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
